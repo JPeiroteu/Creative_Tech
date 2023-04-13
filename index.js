e359100cbf0d90310590fc2aaab59fdf1a400fc6
Jorge Guedes <jorgeguedes16@gmail.com>
2023-04-13 14:45:37 +0100

    routes for events added and code formatted

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ require('dotenv').config();
       app.use(express.urlencoded({ extended: false }));
       app.use(cookieParser(process.env.COOKIE_SECRET));
       app.use(express.static(path.join(__dirname, 'public')));
+
+      const eventsRouter = require("./routes/eventsRoutes");
+      const usersRouter = require("./routes/usersRoutes");
+      
+      app.use("/api/events",eventsRouter);
+      app.use("/api/users",usersRouter);    
+
       const port = parseInt(process.env.port || '8080');
       app.listen(port,function() {
         console.log("Server running at http://localhost:"+port);
