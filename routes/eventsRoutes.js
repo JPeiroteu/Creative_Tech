54b8936c66439cc2f7e046375bc83c822b72e296
Jorge Guedes <jorgeguedes16@gmail.com>
2023-04-13 16:41:50 +0100

    eventsRoutes.js added

---
+++ routes/eventsRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const Event = require("../models/eventsModel");
+
+router.get("/", async function (req, res, next) {
+    try {
+        console.log("Get all events");
+        let result = await Event.getAll();
+        res.status(result.status).send(result.result);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+});
+
+module.exports = router;
