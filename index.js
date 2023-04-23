f31be2fa215271a59c9c60886a998f72c9c98c04
mariaasous <maria.dsousa1998@gmail.com>
2023-04-23 20:18:32 +0100

    fixing events

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const eventsRouter = require("./routes/eventsRoutes");
 const usersRouter = require("./routes/usersRoutes");
 
-app.use("/api/events", eventsRouter);
+app.use("/events", eventsRouter);
 app.use("/api/users", usersRouter);
 
 const port = parseInt(process.env.port || '8080');
