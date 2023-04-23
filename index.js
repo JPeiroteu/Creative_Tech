80391f9bf43bd79d0cc50baf7d8da717fbd16f75
mariaasous <maria.dsousa1998@gmail.com>
2023-04-23 22:16:54 +0100

    events testing

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 const eventsRouter = require("./routes/eventsRoutes");
 const usersRouter = require("./routes/usersRoutes");
 
+app.set('view engine', 'ejs');
+
 app.use("/api/events", eventsRouter);
 app.use("/api/users", usersRouter);
-app.get("/events", function (req, res) {
-  res.sendFile(__dirname + "/public/events.html");
-});
+
 
 
 const port = parseInt(process.env.port || '8080');
