a588b85675704de1b9438b25c55734473fbcd00f
Jorge Guedes <jorgeguedes16@gmail.com>
2023-05-04 17:35:37 +0100

    local use configured

--- config/database.js
+++ config/database.js
@@ -1,6 +1,20 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
+    host: "db4free.net",
+    user: "jpeiro",
+    password: "123peyroteo321",
+    database: "creative_tech",
+    connectionLimit: 20
+});
+
+module.exports = pool;
+
+
+/*
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
@@ -9,3 +23,4 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool;
+*/
