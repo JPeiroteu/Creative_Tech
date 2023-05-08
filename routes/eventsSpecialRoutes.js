1f7b4412e460d1247885a9ecaf8bac2526d18255
Jorge Guedes <jorgeguedes16@gmail.com>
2023-05-08 16:35:07 +0100

    SpecialEvent fixed

--- routes/eventsSpecialRoutes.js
+++ routes/eventsSpecialRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const Event = require("../models/eventsModel").SpecialEvent;
+const SpecialEvent = require("../models/eventsModel").SpecialEvent;
 
 router.get("/", async function (req, res, next) {
   try {
     console.log("Get all events");
-    let result = await Event.allSpecialEvents();
+    let result = await SpecialEvent.allSpecialEvents();
     res.status(result.status).send(result.result);
   } catch (err) {
     console.log(err);
