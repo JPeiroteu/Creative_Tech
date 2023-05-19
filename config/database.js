e98c56d0a7dbdc80e9ad29b741b39168fc768802
Jorge Guedes <jorgeguedes16@gmail.com>
2023-05-19 12:12:13 +0100

    events total participants fixed

--- config/database.js
+++ config/database.js
@@ -1,11 +1,21 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
+    host: "db4free.NET",
+    user: "jpeiro",
+    password: "123peyroteo321",
+    database: "creative_tech",
+    connectionLimit: 20
+});
+
+/*
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     connectionLimit: 20
 });
+*/
 
 module.exports = pool;
--- models/eventsModel.js
+++ models/eventsModel.js
@@ -1,5 +1,15 @@
 const pool = require("../config/database");
 
+function eventFromDB(dbObj) {
+  return {
+    id: dbObj.event_id,
+    name: dbObj.event_name,
+    date: dbObj.event_date,
+    location: dbObj.event_location,
+    description: dbObj.event_description,
+  };
+}
+
 class Event {
   constructor(id, name, date, location, description) {
     this.id = id;
@@ -12,40 +22,22 @@ class Event {
   static async allEvents() {
     try {
       let result = [];
-      let [rows, _] = await pool.query("SELECT id, name, date, location, description FROM Events");
-      for (let row of rows) {
-        let event = new Event(row.id, row.name, row.date, row.location, row.description);
+      let [dbEvents, fields] = await pool.query("SELECT * FROM Events");
+      for (let dbEvent of dbEvents) {
+        let event = eventFromDB(dbEvent);
+        // Obter o número total de participantes para cada evento
+        const query = 'SELECT COUNT(*) as attendees FROM User_Events WHERE event_id = ? AND is_going = 1';
+        const [rows, _] = await pool.query(query, [dbEvent.event_id]);
+        event.attendees = rows[0].attendees;
         result.push(event);
       }
+      result.reverse();
       return { status: 200, result: result };
     } catch (err) {
       console.log(err);
-      return { status: 500, result: [] };
+      return { status: 500, result: err };
     }
   }
 }
 
-class SpecialEvent extends Event {
-  constructor(id, name, date, location, description, maxParticipants, price) {
-    super(id, name, date, location, description);
-    this.maxParticipants = maxParticipants;
-    this.price = price;
-  }
-
-  static async allSpecialEvents() {
-    try {
-      let result = [];
-      let [rows, _] = await pool.query("SELECT id, name, date, location, description, maxParticipants, price FROM Events WHERE maxParticipants IS NOT NULL AND price IS NOT NULL");
-      for (let row of rows) {
-        let specialEvent = new SpecialEvent(row.id, row.name, row.date, row.location, row.description, row.maxParticipants, row.price);
-        result.push(specialEvent);
-      }
-      return { status: 200, result: result };
-    } catch (err) {
-      console.log(err);
-      return { status: 500, result: [] };
-    }
-  }
-}
-
-module.exports = { Event, SpecialEvent };
+module.exports = Event;
